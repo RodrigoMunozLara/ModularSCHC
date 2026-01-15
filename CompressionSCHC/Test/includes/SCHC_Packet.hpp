383e7a4d2bc329923127c50bafd6ee88cef650ea
Fabian <fabian.clavijo@usm.cl>
2026-01-15 16:47:30 -0300

    Creación de clase para paquete SCHC, junto con paquetes a comprimir a SCHC

---
+++ CompressionSCHC/Test/includes/SCHC_Packet.hpp
@@ -0,0 +1,28 @@
+/*
+SCHC Packet Definition
+This header file defines the structures and enumerations necessary for representing SCHC (Static Context Header Compression) packets.
+*/
+
+#include <cstdint>
+#include <vector>
+#pragma once
+//+------------------------------------------------------------------------------------------------------+
+
+class SCHC_Compressed_Packet {
+    private:
+        uint8_t compressionRuleID; //Compression Rule ID. 1 byte
+        std::vector <uint16_t> compressionResidue; //Compression Residue. 4 bytes for item. Dinamic size
+        uint8_t padding; //Padding to align to byte boundary
+        
+
+    public:
+        SCHC_Compressed_Packet(); //Default Constructor
+        SCHC_Compressed_Packet(uint8_t rule, std::vector<uint16_t> residue); //Constructor with parameters
+        ~SCHC_Compressed_Packet(); //Destructor
+
+        uint32_t getPacketLength(); //Getter for packet length
+
+        void setPacketData(uint8_t rule, std::vector<uint16_t> residue, uint8_t padding); //Setter for packet data and length
+
+        void printPacket() const; //Print Packet Details
+};
